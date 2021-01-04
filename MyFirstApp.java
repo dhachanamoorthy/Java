c0fef4e319f8fb0963c78efe3cfb10d9411d096f
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2021-01-04 23:38:49 +0530

    Declarations and Assignments

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -2,5 +2,12 @@
 public class MyFirstApp{
     public static void main(String[] args) {
        System.out.println("I am Moorthy"); 
+       //Declaration
+        int a;
+        String b;
+        //Assignment
+        a=10;
+        b="moorthy";
+        System.out.println("a="+a+" b="+b);
    } 
 }
