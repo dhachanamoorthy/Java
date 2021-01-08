439f8acf38721c6127e6f472fcd06e66a59625f1
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2021-01-08 09:45:35 +0530

    Creating Object and methods and returning data from methodes

--- Objects/MainMovieObject.java
+++ Objects/MainMovieObject.java
@@ -5,6 +5,16 @@ class Movie{
     void playIt(){
         System.out.println("playing movie "+title);
     }
+    //creating method with argument
+    void setRating(int rate){
+        rating=rate;
+    }
+
+    //creating method with return value
+
+    String getMovieTitle(){
+        return title;
+    }
 }
 public class MainMovieObject{
     public static void main(String args[]) {
@@ -14,12 +24,19 @@ public class MainMovieObject{
         //initializing object attributes value
         a1.title="Master";
         a1.genre="Commercial";
-        a1.rating=8;
         a2.title="Tamil Padam";
         a2.genre="Comedy";
-        a2.rating=6;
+
+        //passing argument to the function 
+        a1.setRating(8);
+        a2.setRating(6);
+
+
         //calling methods
         a1.playIt();
         a2.playIt();
+
+        //printing returned value from method
+        System.out.println(a1.getMovieTitle());
     }
 }
