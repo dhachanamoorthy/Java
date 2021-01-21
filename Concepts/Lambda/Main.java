197154f01f9b9635adcfc088cceae29016629f5d
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2021-01-21 15:16:08 +0530

    Program on Lambda Funtion

---
+++ Concepts/Lambda/Main.java
@@ -0,0 +1,19 @@
+//Lambda Expressions
+
+import java.util.ArrayList; 
+
+public class Main{
+    public static void main(String[] args) {
+
+        ArrayList<Integer> al=new ArrayList<Integer>();
+
+        //Adding data to the object
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+        al.add(5);
+        //((parameter)->{code to be excuted});
+        al.forEach( (n) -> {System.out.println(n);});
+    }
+}
