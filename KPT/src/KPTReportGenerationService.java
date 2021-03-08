61c487fde94f0984cc5243fd3d9fc83f1bcac5e5
dhacahanamoothy <dhacahanamoorthy3@gmail.com>
2021-03-07 20:24:21 -0500

    Initial commit

---
+++ KPT/src/KPTReportGenerationService.java
@@ -0,0 +1,35 @@
+class KPTReportGeneration{
+    private int hospital_id;
+    private int rep_id;
+    private int no_of_tray_used_per_day;
+    private int no_of_tray_used_per_month;
+    private int total_payout_by_rep_per_day;
+    private int total_payout_by_rep_per_month;
+    private int total_commission_for_kpt_per_day;
+    private int total_commission_for_kpt_per_month;
+
+    public void calculateNoOfTrayUsedByHopitalPerDay(){
+
+        
+    }
+    public void calculateNoOfTrayUsedByHopitalPerMonth(){
+
+        
+    }
+    public void calculateTotalPayoutByRepPerDay(){
+
+        
+    }
+    public void calculateTotalPayoutByRepPerMonth(){
+
+        
+    }
+
+    public void calculateCommissionForKPTPerDay(){
+
+    }
+
+    public void calculateCommissionForKPTPerMonth(){
+
+    } 
+}
