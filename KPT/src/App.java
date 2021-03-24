2bc296f197f1b8f480e264dc3f6137b560b1f13e
dhachanamoorthy <dhachanamoorthy3@gmail.com>
2021-03-24 06:30:41 +0530

    Created yearly report generation Class and added to App

--- KPT/src/App.java
+++ KPT/src/App.java
@@ -1,8 +1,14 @@
 import service.DataProcessing;
 import model.*;
+import report.*;
 public class App {
     public static void main(String[] args) throws Exception { 
         DataProcessing dataProcessing=new DataProcessing(); 
+        
         dataProcessing.process();
+
+        Yearly yearly=new Yearly();
+        yearly.getRepPayout();
+        yearly.getHospitalTrayUsage();
     }
 }
